81c19dee6be5c13e1bcd401b5fae0d7fe8ebfb49
pixeegithub-testuser2 <120673387+pixeegithub-testuser2@users.noreply.github.com>
2026-09-14 16:45:47 -0700

    :alembic: Example commit to make this PR and any analysis that operates on it unique.

--- introduction/other_views.py
+++ introduction/other_views.py
@@ -50,13 +50,13 @@ def cmd_lab3(request):
             os=request.POST.get('os')
             print(os)
             if(os=='win'):
-                command="nslookup {}".format(domain)
+                command_mP_4ywAr="nslookup {}".format(domain)
             else:
-                command = "dig {}".format(domain)
+                command_mP_4ywAr = "dig {}".format(domain)
             try:
-                # output=subprocess.check_output(command,shell=True,encoding="UTF-8")
+                # output=subprocess.check_output(command_mP_4ywAr,shell=True,encoding="UTF-8")
                 process = subprocess.Popen(
-                    command,
+                    command_mP_4ywAr,
                     shell=True,
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE)
